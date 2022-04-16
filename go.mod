7f38db665096396cfaac3dddb0bfced906626eb9
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2022-04-16 16:01:55 -0300

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/gviegas/scene
+
+go 1.18
+
+require github.com/gviegas/gpu v0.1.0
---
+++ scene.go
@@ -0,0 +1,7 @@
+// Copyright 2022 Gustavo C. Viegas. All rights reserved.
+
+// Package scene provides functionality for creating and
+// rendering scene graphs.
+package scene
+
+import ()
