c2e2b22a26aa697c80aee9096ff37ffe652fa231
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2022-04-22 15:57:04 -0300

    linear/vector.go: refactored to use methods and pointers

--- linear/linear_test.go
+++ linear/linear_test.go
@@ -8,47 +8,48 @@ import (
 )
 
 func TestV(t *testing.T) {
+	var u V3
 	v := V3{1, 2, 4}
 	w := V3{0, -1, 2}
 
-	if u := AddV3(v, w); u != (V3{1, 1, 6}) {
-		t.Fatalf("AddV3\nhave %v\nwant [1 1 6]", u)
+	if u.Add(&v, &w); u != (V3{1, 1, 6}) {
+		t.Fatalf("V3.Add\nhave %v\nwant [1 1 6]", u)
 	}
-	if u := SubV3(v, w); u != (V3{1, 3, 2}) {
-		t.Fatalf("SubV3\nhave %v\nwant [1 3 2]", u)
+	if u.Sub(&v, &w); u != (V3{1, 3, 2}) {
+		t.Fatalf("V3.Sub\nhave %v\nwant [1 3 2]", u)
 	}
-	if u := ScaleV3(-1, v); u != (V3{-1, -2, -4}) {
-		t.Fatalf("ScaleV3\nhave %v\nwant [-1 -2 -4]", u)
+	if u.Scale(-1, &v); u != (V3{-1, -2, -4}) {
+		t.Fatalf("V3.Scale\nhave %v\nwant [-1 -2 -4]", u)
 	}
-	if u := ScaleV3(2, w); u != (V3{0, -2, 4}) {
-		t.Fatalf("ScaleV3\nhave %v\nwant [0 -2 4]", u)
+	if u.Scale(2, &w); u != (V3{0, -2, 4}) {
+		t.Fatalf("V3.Scale\nhave %v\nwant [0 -2 4]", u)
 	}
-	if d := DotV3(v, w); d != 6 {
-		t.Fatalf("DotV3\nhave %v\nwant 6\n", d)
+	if d := v.Dot(&w); d != 6 {
+		t.Fatalf("V3.Dot\nhave %v\nwant 6\n", d)
 	}
-	if d := DotV3(v, v); d != 21 {
-		t.Fatalf("DotV3\nhave %v\nwant 21\n", d)
+	if d := v.Dot(&v); d != 21 {
+		t.Fatalf("V3.Dot\nhave %v\nwant 21\n", d)
 	}
-	if l := LenV3(v); l != float32(math.Sqrt(21)) {
-		t.Fatalf("LenV3\nhave %v\nwant %v\n", l, math.Sqrt(21))
+	if l := v.Len(); l != float32(math.Sqrt(21)) {
+		t.Fatalf("V3.Len\nhave %v\nwant %v\n", l, math.Sqrt(21))
 	}
-	if l := LenV3(w); l != float32(math.Sqrt(5)) {
-		t.Fatalf("LenV3\nhave %v\nwant %v\n", l, math.Sqrt(5))
+	if l := w.Len(); l != float32(math.Sqrt(5)) {
+		t.Fatalf("V3.Len\nhave %v\nwant %v\n", l, math.Sqrt(5))
 	}
 
 	v = V3{0, 0, -2}
 	w = V3{0, 4, 0}
 
-	if v = NormV3(v); v != (V3{0, 0, -1}) {
-		t.Fatalf("NormV3\nhave %v\nwant [0 0 -1]", v)
+	if v.Norm(&v); v != (V3{0, 0, -1}) {
+		t.Fatalf("V3.Norm\nhave %v\nwant [0 0 -1]", v)
 	}
-	if w = NormV3(w); w != (V3{0, 1, 0}) {
-		t.Fatalf("NormV3\nhave %v\nwant [0 1 0]", w)
+	if w.Norm(&w); w != (V3{0, 1, 0}) {
+		t.Fatalf("V3.Norm\nhave %v\nwant [0 1 0]", w)
 	}
-	if u := Cross(v, w); u != (V3{1, 0, 0}) {
-		t.Fatalf("Cross\nhave %v\nwant [1 0 0]", u)
+	if u.Cross(&v, &w); u != (V3{1, 0, 0}) {
+		t.Fatalf("V3.Cross\nhave %v\nwant [1 0 0]", u)
 	}
-	if u := Cross(w, v); u != (V3{-1, 0, 0}) {
-		t.Fatalf("Cross\nhave %v\nwant [-1 0 0]", u)
+	if u.Cross(&w, &v); u != (V3{-1, 0, 0}) {
+		t.Fatalf("V3.Cross\nhave %v\nwant [-1 0 0]", u)
 	}
 }
--- linear/vector.go
+++ linear/vector.go
@@ -10,97 +10,83 @@ import (
 // V3 is a 3-component vector of float32.
 type V3 [3]float32
 
-// AddV3 returns v + w.
-func AddV3(v, w V3) (u V3) {
-	for i := range u {
-		u[i] = v[i] + w[i]
+// Add sets v to contain l + r.
+func (v *V3) Add(l, r *V3) {
+	for i := range v {
+		v[i] = l[i] + r[i]
 	}
-	return
 }
 
-// SubV3 returns v - w.
-func SubV3(v, w V3) (u V3) {
-	for i := range u {
-		u[i] = v[i] - w[i]
+// Sub sets v to contain l - r.
+func (v *V3) Sub(l, r *V3) {
+	for i := range v {
+		v[i] = l[i] - r[i]
 	}
-	return
 }
 
-// ScaleV3 returns s ⋅ v.
-func ScaleV3(s float32, v V3) (u V3) {
-	for i := range u {
-		u[i] = s * v[i]
+// Scale sets v to contain s ⋅ w.
+func (v *V3) Scale(s float32, w *V3) {
+	for i := range v {
+		v[i] = s * w[i]
 	}
-	return
 }
 
-// DotV3 returns v ⋅ w.
-func DotV3(v, w V3) (d float32) {
+// Dot returns v ⋅ w.
+func (v *V3) Dot(w *V3) (d float32) {
 	for i := range v {
 		d += v[i] * w[i]
 	}
 	return
 }
 
-// LenV3 returns the length of v.
-func LenV3(v V3) float32 {
-	return float32(math.Sqrt(float64(DotV3(v, v))))
-}
+// Len returns the length of v.
+func (v *V3) Len() float32 { return float32(math.Sqrt(float64(v.Dot(v)))) }
 
-// NormV3 returns v normalized.
-func NormV3(v V3) V3 {
-	return ScaleV3(1/LenV3(v), v)
-}
+// Norm sets v to contain w normalized.
+func (v *V3) Norm(w *V3) { v.Scale(1/w.Len(), w) }
 
-// Cross returns v × w.
-func Cross(v, w V3) (u V3) {
-	u[0] = v[1]*w[2] - v[2]*w[1]
-	u[1] = v[2]*w[0] - v[0]*w[2]
-	u[2] = v[0]*w[1] - v[1]*w[0]
+// Cross sets v to contain l × r.
+func (v *V3) Cross(l, r *V3) {
+	v[0] = l[1]*r[2] - l[2]*r[1]
+	v[1] = l[2]*r[0] - l[0]*r[2]
+	v[2] = l[0]*r[1] - l[1]*r[0]
 	return
 }
 
 // V4 is a 4-component vector of float32.
 type V4 [4]float32
 
-// AddV4 returns v + w.
-func AddV4(v, w V4) (u V4) {
-	for i := range u {
-		u[i] = v[i] + w[i]
+// Add sets v to contain l + r.
+func (v *V4) Add(l, r *V4) {
+	for i := range v {
+		v[i] = l[i] + r[i]
 	}
-	return
 }
 
-// SubV4 returns v - w.
-func SubV4(v, w V4) (u V4) {
-	for i := range u {
-		u[i] = v[i] - w[i]
+// Sub sets v to contain l - r.
+func (v *V4) Sub(l, r *V4) {
+	for i := range v {
+		v[i] = l[i] - r[i]
 	}
-	return
 }
 
-// ScaleV4 returns s ⋅ v.
-func ScaleV4(s float32, v V4) (u V4) {
-	for i := range u {
-		u[i] = s * v[i]
+// Scale sets v to contain s ⋅ w.
+func (v *V4) Scale(s float32, w *V4) {
+	for i := range v {
+		v[i] = s * w[i]
 	}
-	return
 }
 
-// DotV4 returns v ⋅ w.
-func DotV4(v, w V4) (d float32) {
+// Dot returns v ⋅ w.
+func (v *V4) Dot(w *V4) (d float32) {
 	for i := range v {
 		d += v[i] * w[i]
 	}
 	return
 }
 
-// LenV4 returns the length of v.
-func LenV4(v V4) float32 {
-	return float32(math.Sqrt(float64(DotV4(v, v))))
-}
+// Len returns the length of v.
+func (v *V4) Len() float32 { return float32(math.Sqrt(float64(v.Dot(v)))) }
 
-// NormV4 returns v normalized.
-func NormV4(v V4) V4 {
-	return ScaleV4(1/LenV4(v), v)
-}
+// Norm sets v to contain w normalized.
+func (v *V4) Norm(w *V4) { v.Scale(1/w.Len(), w) }
