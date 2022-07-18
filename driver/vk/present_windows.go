70b15edfee158e3e2807c6a88f237ca279eeb2bf
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2022-07-18 16:24:09 -0300

    driver/vk/present_windows.go: implemented initWin32Surface method

--- driver/vk/present_windows.go
+++ driver/vk/present_windows.go
@@ -2,6 +2,7 @@
 
 package vk
 
+// #include <windows.h>
 // #include <proc.h>
 import "C"
 
@@ -17,10 +18,26 @@ func (s *swapchain) initSurface() error {
 	return driver.ErrCannotPresent
 }
 
-// TODO
 func (s *swapchain) initWin32Surface() error {
 	if !s.d.exts[extWin32Surface] {
 		return driver.ErrCannotPresent
 	}
-	panic("not implemented")
+	info := C.VkWin32SurfaceCreateInfoKHR{
+		sType:     C.VK_STRUCTURE_TYPE_WIN32_SURFACE_CREATE_INFO_KHR,
+		hinstance: C.HINSTANCE(wsi.HinstWin32()),
+		hwnd:      C.HWND(wsi.HwndWin32(s.win)),
+	}
+	var sf C.VkSurfaceKHR
+	err := checkResult(C.vkCreateWin32SurfaceKHR(s.d.inst, &info, nil, &sf))
+	if err != nil {
+		return err
+	}
+	qfam, err := s.d.presQueueFor(sf)
+	if err != nil {
+		C.vkDestroySurfaceKHR(s.d.inst, sf, nil)
+		return err
+	}
+	s.qfam = qfam
+	s.sf = sf
+	return nil
 }
