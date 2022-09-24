d754fb6d5c012c643c932284a442d600780665f1
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2022-09-24 15:20:56 -0300

    gltf/gltf_test.go: replaced TestGLB with TestIsGLB

--- gltf/gltf_test.go
+++ gltf/gltf_test.go
@@ -41,7 +41,7 @@ func TestGLTF(t *testing.T) {
 	t.Log(string(buf.Bytes()))
 }
 
-func TestGLB(t *testing.T) {
+func TestIsGLB(t *testing.T) {
 	file, err := os.Open("testdata/cube.glb")
 	if err != nil {
 		t.Fatal(err)
@@ -53,60 +53,6 @@ func TestGLB(t *testing.T) {
 	r := bytes.NewReader([]byte(`{"asset:"{"version":"2.0"}}`))
 	if IsGLB(r) {
 		t.Fatal("IsGLB(r):\nwant false\nhave true")
-	}
-
-	// SeekJSON
-	//file.Seek(0, 0)
-	n, err := SeekJSON(file, 1)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if n <= 0 {
-		t.Fatal("SeekJSON(file):\nwant n > 0\nhave 0")
-	}
-	b := make([]byte, n)
-	n, err = file.Read(b)
-	if err != nil {
-		t.Fatal(err)
-	}
-	gltf, err := Decode(bytes.NewReader(b))
-	if err != nil {
-		t.Fatal(err)
-	}
-	var buf bytes.Buffer
-	err = Encode(&buf, gltf)
-	if err != nil {
-		t.Fatal(err)
-	}
-	b = append(b[:0], buf.Bytes()...)
-	buf.Reset()
-	err = json.Indent(&buf, b, "", "    ")
-	if err != nil {
-		t.Fatal(err)
-	}
-	t.Log(string(buf.Bytes()))
-
-	// SeekBIN
-	//file.Seek(0, 0)
-	n, err = SeekBIN(file, 1)
-	if err != nil {
-		t.Fatal(err)
-	}
-	want := gltf.Buffers[0].ByteLength
-	if pad := want % 4; pad != 0 {
-		want += 4 - pad
-	}
-	if want != int64(n) {
-		t.Fatalf("SeekBIN(file):\nwant n == %d\nhave n == %d", want, n)
-	}
-	if n > len(b) {
-		b = make([]byte, n)
-	} else {
-		b = b[:n]
-	}
-	n, err = file.Read(b)
-	if err != nil {
-		t.Fatal(err)
 	}
 }
 
