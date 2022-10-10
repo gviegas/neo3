be8d8e94e3c131a1025168c838ccf541a13c7d80
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2022-10-10 00:09:23 -0300

    new file: wsi/wsi_wayland.go

---
+++ wsi/wsi_wayland.go
@@ -0,0 +1,84 @@
+// Copyright 2022 Gustavo C. Viegas. All rights reserved.
+
+//go:build linux && !android
+
+package wsi
+
+// initWayland initializes the Wayland platform.
+func initWayland() error {
+	// TODO
+	panic("not implemented")
+}
+
+// deinitWayland deinitializes the Wayland platform.
+func deinitWayland() {
+	// TODO
+	panic("not implemented")
+}
+
+// windowWayland implements Window.
+type windowWayland struct {
+	// TODO
+	width  int
+	height int
+	title  string
+	mapped bool
+}
+
+// newWindowWayland creates a new window.
+func newWindowWayland(width, height int, title string) (Window, error) {
+	// TODO
+	panic("not implemented")
+}
+
+// Map makes the window visible.
+func (w *windowWayland) Map() error {
+	// TODO
+	panic("not implemented")
+}
+
+// Unmap hides the window.
+func (w *windowWayland) Unmap() error {
+	// TODO
+	panic("not implemented")
+}
+
+// Resize resizes the window.
+func (w *windowWayland) Resize(width, height int) error {
+	// TODO
+	panic("not implemented")
+}
+
+// SetTitle sets the window's title.
+func (w *windowWayland) SetTitle(title string) error {
+	// TODO
+	panic("not implemented")
+}
+
+// Close closes the window.
+func (w *windowWayland) Close() {
+	// TODO
+	panic("not implemented")
+}
+
+// Width returns the window's width.
+func (w *windowWayland) Width() int { return w.width }
+
+// Height returns the window's height.
+func (w *windowWayland) Height() int { return w.height }
+
+// Title returns the window's title.
+func (w *windowWayland) Title() string { return w.title }
+
+// dispatchWayland dispatches queued events.
+func dispatchWayland() {
+	// TODO
+	panic("not implemented")
+}
+
+// setAppNameWayland updates the string used to identify the
+// application.
+func setAppNameWayland(s string) {
+	// TODO
+	panic("not implemented")
+}
