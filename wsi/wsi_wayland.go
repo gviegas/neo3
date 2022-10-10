251cc99418004cab29cd0fe21f4493dd9b1c11d4
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2022-10-10 11:01:56 -0300

    wsi/wsi_wayland.go: shared lib open/close & symbol fetch

--- wsi/wsi_wayland.go
+++ wsi/wsi_wayland.go
@@ -4,6 +4,53 @@
 
 package wsi
 
+// #cgo linux LDFLAGS: -ldl
+// #include <dlfcn.h>
+// #include <stdlib.h>
+// #include <wsi_wayland.h>
+import "C"
+
+import (
+	"errors"
+	"unsafe"
+)
+
+// Handle for the shared object.
+var hWayland unsafe.Pointer
+
+// openWayland opens the shared library and gets function pointers.
+// It is not safe to call any of the C wrappers unless this
+// function succeeds.
+func openWayland() error {
+	if hWayland == nil {
+		lib := C.CString("libwayland-client.so.0")
+		defer C.free(unsafe.Pointer(lib))
+		hWayland := C.dlopen(lib, C.RTLD_LAZY|C.RTLD_GLOBAL)
+		if hWayland == nil {
+			return errors.New("wsi: failed to open libwayland")
+		}
+		for i := range C.nameWayland {
+			C.ptrWayland[i] = C.dlsym(hWayland, C.nameWayland[i])
+			if C.ptrWayland[i] == nil {
+				C.dlclose(hWayland)
+				hWayland = nil
+				return errors.New("wsi: failed to fetch Wayland symbol")
+			}
+		}
+	}
+	return nil
+}
+
+// closeWayland closes the shared library.
+// It is not safe to call any of the C wrappers after
+// calling this function.
+func closeWayland() {
+	if hWayland != nil {
+		C.dlclose(hWayland)
+		hWayland = nil
+	}
+}
+
 // initWayland initializes the Wayland platform.
 func initWayland() error {
 	// TODO
