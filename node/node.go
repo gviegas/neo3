194d90e589db6595439e0eb7e3c7289759777690
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2023-02-02 18:24:33 -0300

    node/node.go: Graph.Insert method

--- node/node.go
+++ node/node.go
@@ -49,11 +49,49 @@ type Graph struct {
 }
 
 // Insert inserts a new node as descendant of prev.
-func (g *Graph) Insert(node Interface, prev Node) Node {
-	panic("not implemented")
+func (g *Graph) Insert(n Interface, prev Node) Node {
+	if g.nodeMap.Rem() == 0 {
+		// TODO: Grow exponentially.
+		var elems [32]node
+		g.nodes = append(g.nodes, elems[:]...)
+		g.nodeMap.Grow(1)
+	}
+	var newn Node
+	if idx, ok := g.nodeMap.Search(); ok {
+		g.nodeMap.Set(idx)
+		newn = Node(idx + 1)
+	} else {
+		// Should never happen.
+		panic("unexpected failure from bitm.Bitm.Search")
+	}
+	if prev != Nil {
+		if sub := g.nodes[prev-1].sub; sub != Nil {
+			g.nodes[newn-1].next = sub
+			g.nodes[sub-1].prev = newn
+		} else {
+			g.nodes[newn-1].next = Nil
+		}
+		g.nodes[newn-1].prev = prev
+		g.nodes[prev-1].sub = newn
+	} else {
+		if g.next != Nil {
+			g.nodes[g.next-1].prev = newn
+			g.nodes[newn-1].next = g.next
+		} else {
+			g.nodes[newn-1].next = Nil
+		}
+		g.nodes[newn-1].prev = Nil
+		g.next = newn
+	}
+	g.nodes[newn-1].sub = Nil
+	g.nodes[newn-1].data = len(g.data)
+	var world linear.M4
+	world.I()
+	g.data = append(g.data, data{n, world, newn})
+	return newn
 }
 
 // Remove removes a node.
-func (g *Graph) Remove(node Node) Interface {
+func (g *Graph) Remove(n Node) Interface {
 	panic("not implemented")
 }
