9c53952ed6f7cd48741f8a13e2472b03534aa5e4
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2023-03-29 17:21:15 -0300

    new file: engine/internal/shader/desc.go

---
+++ engine/internal/shader/desc.go
@@ -0,0 +1,127 @@
+// Copyright 2023 Gustavo C. Viegas. All rights reserved.
+
+// Descriptor management.
+//
+// For portability, the following restrictions apply:
+//
+//	DescHeap per DescTable           | 4 (max)
+//	DTexture/DSampler descriptors    | 16 (max)
+//	DConstant descriptors            | 12 (max)
+//	DImage/DBuffer descriptors       | 4 (max)
+//	DConstant/DBuffer data alignment | 256 bytes (min)
+//	DConstant/DBuffer data size      | 16 KiB (max)
+//
+// (the above names refer to the driver package).
+
+package shader
+
+import (
+	"github.com/gviegas/scene/driver"
+	"github.com/gviegas/scene/engine/internal/ctxt"
+)
+
+func constantDesc(nr int) driver.Descriptor {
+	return driver.Descriptor{
+		Type:   driver.DConstant,
+		Stages: driver.SVertex | driver.SFragment,
+		Nr:     nr,
+		Len:    1,
+	}
+}
+
+func textureDesc(nr int) driver.Descriptor {
+	return driver.Descriptor{
+		Type:   driver.DTexture,
+		Stages: driver.SVertex | driver.SFragment,
+		Nr:     nr,
+		Len:    1,
+	}
+}
+
+func samplerDesc(nr int) driver.Descriptor {
+	return driver.Descriptor{
+		Type:   driver.DSampler,
+		Stages: driver.SVertex | driver.SFragment,
+		Nr:     nr,
+		Len:    1,
+	}
+}
+
+// newDescHeap0 creates a new driver.DescHeap suitable for
+// frame (FrameLayout) and light (LightLayout) data.
+func newDescHeap0() (driver.DescHeap, error) {
+	return ctxt.GPU().NewDescHeap([]driver.Descriptor{
+		// Frame.
+		constantDesc(0),
+		// Light.
+		constantDesc(1),
+	})
+}
+
+// newDescHeap1 creates a new driver.DescHeap suitable for
+// drawable (DrawableLayout) data.
+func newDescHeap1() (driver.DescHeap, error) {
+	return ctxt.GPU().NewDescHeap([]driver.Descriptor{
+		constantDesc(0),
+	})
+}
+
+// newDescHeap2 creates a new driver.DescHeap suitable for
+// material (MaterialLayout) data plus texture/samplers.
+func newDescHeap2() (driver.DescHeap, error) {
+	return ctxt.GPU().NewDescHeap([]driver.Descriptor{
+		constantDesc(0),
+		// Base color.
+		textureDesc(1), samplerDesc(2),
+		// Metallic-roughness.
+		textureDesc(3), samplerDesc(4),
+		// Normal map.
+		textureDesc(5), samplerDesc(6),
+		// Occlusion map.
+		textureDesc(7), samplerDesc(8),
+		// Emissive map.
+		textureDesc(9), samplerDesc(10),
+	})
+}
+
+// newDescHeap3 creates a new driver.DescHeap suitable for
+// joint (JointLayout) data.
+func newDescHeap3() (driver.DescHeap, error) {
+	return ctxt.GPU().NewDescHeap([]driver.Descriptor{
+		constantDesc(0),
+	})
+}
+
+// newDescTable creates a new driver.DescTable containing
+// the heaps 0-3.
+// This table is used for drawing shaded models.
+func newDescTable() (driver.DescTable, error) {
+	dh0, err := newDescHeap0()
+	if err != nil {
+		return nil, err
+	}
+	dh1, err := newDescHeap1()
+	if err != nil {
+		dh0.Destroy()
+		return nil, err
+	}
+	dh2, err := newDescHeap2()
+	if err != nil {
+		dh0.Destroy()
+		dh1.Destroy()
+		return nil, err
+	}
+	dh3, err := newDescHeap3()
+	if err != nil {
+		dh0.Destroy()
+		dh1.Destroy()
+		dh2.Destroy()
+		return nil, err
+	}
+	return ctxt.GPU().NewDescTable([]driver.DescHeap{
+		dh0,
+		dh1,
+		dh2,
+		dh3,
+	})
+}
