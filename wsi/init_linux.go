dbcc1fa636e00c818f3b367509a3c3cd3cb5bff9
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2023-06-07 14:49:56 -0300

    wsi/init_linux.go: default to Xorg for the time being

--- wsi/init_linux.go
+++ wsi/init_linux.go
@@ -9,7 +9,9 @@ import (
 )
 
 func init() {
-	if os.Getenv("WAYLAND_DISPLAY") != "" {
+	// TODO: Prefer Xorg for now since Wayland has no window decorations.
+	_, useWL := os.LookupEnv("DO_USE_WAYLAND")
+	if useWL && os.Getenv("WAYLAND_DISPLAY") != "" {
 		if err := initWayland(); err != nil {
 			os.Stderr.WriteString(err.Error() + "\n")
 		} else {
