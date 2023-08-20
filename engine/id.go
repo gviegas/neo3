eda44b111556536e96e6df5879501749e23c20f2
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2023-08-20 10:56:27 -0300

    engine/id.go: add dataMap.insert method

--- engine/id.go
+++ engine/id.go
@@ -24,3 +24,31 @@ type dataMap[I ~int, D any] struct {
 	idMap bitm.Bitm[uint32]
 	data  []dataEntry[D]
 }
+
+// insert inserts data into m.
+// It returns an I value that identifies data in m.
+func (m *dataMap[I, D]) insert(data D) I {
+	if m.idMap.Rem() == 0 {
+		switch n := m.idMap.Len(); {
+		case n > 0:
+			cnt := 1 + (n-31)/32
+			m.ids = append(m.ids, m.ids...)
+			m.idMap.Grow(cnt)
+		default:
+			var elems [32]dataID
+			m.ids = append(m.ids, elems[:]...)
+			m.idMap.Grow(1)
+		}
+	}
+	var id I
+	if idx, ok := m.idMap.Search(); ok {
+		m.idMap.Set(idx)
+		id = I(idx)
+	} else {
+		// Should never happen.
+		panic("unexpected failure from bitm.Bitm.Search")
+	}
+	m.ids[id] = dataID{data: len(m.data)}
+	m.data = append(m.data, dataEntry[D]{data, int(id)})
+	return id
+}
