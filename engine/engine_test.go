964004d98b07074c8504e20a60c9440421ee24a4
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2023-08-20 17:28:04 -0300

    engine/engine_test.go: add TestID func

--- engine/engine_test.go
+++ engine/engine_test.go
@@ -4,6 +4,7 @@ package engine
 
 import (
 	"math"
+	"strconv"
 	"testing"
 
 	"gviegas/neo3/engine/internal/ctxt"
@@ -28,6 +29,169 @@ func TestCtxt(t *testing.T) {
 	if u, err := drv.Open(); u != gpu || err != nil {
 		t.Fatalf("drv.Open: ctxt mismatch\nhave %v, %v\nwant %v, nil", u, err, gpu)
 	}
+}
+
+func TestID(t *testing.T) {
+	type id int
+	var m dataMap[id, string]
+
+	checkGet := func(id id, want string) {
+		if data := m.get(id); *data != want {
+			t.Fatalf("dataMap.get\nhave %s\nwant %s", *data, want)
+		}
+	}
+	checkLen := func(want int) {
+		if len := m.len(); len != want {
+			t.Fatalf("dataMap.len()\nhave %d\nwant %d", len, want)
+		}
+	}
+	checkInsert := func(data string) id {
+		len := m.len()
+		id := m.insert(data)
+		checkGet(id, data)
+		checkLen(len + 1)
+		return id
+	}
+	checkRemove := func(id id, want string) string {
+		len := m.len()
+		data := m.remove(id)
+		if data != want {
+			t.Fatalf("dataMap.remove()\nhave %s\nwant %s", data, want)
+		}
+		checkLen(len - 1)
+		return data
+	}
+
+	var id0, id1, id2, id3 id
+	var data0 string
+
+	checkLen(0)
+	id0 = checkInsert("hi")
+	checkGet(id0, "hi")
+	data0 = checkRemove(id0, "hi")
+	id0 = checkInsert(data0)
+	checkGet(id0, data0)
+	checkRemove(id0, data0)
+
+	checkLen(0)
+	id0 = checkInsert("bye")
+	id1 = checkInsert("bye bye")
+	checkGet(id1, "bye bye")
+	checkGet(id0, "bye")
+	data0 = checkRemove(id0, "bye")
+	id0 = checkInsert(data0)
+	checkGet(id1, "bye bye")
+	checkGet(id0, "bye")
+	id2 = checkInsert(data0)
+	checkRemove(id1, "bye bye")
+	checkRemove(id0, data0)
+	checkRemove(id2, data0)
+
+	checkLen(0)
+	id0, id1, id2, id3 = checkInsert("a"), checkInsert("b"), checkInsert("c"), checkInsert("d")
+	checkRemove(id2, "c")
+	id2 = checkInsert("e")
+	checkGet(id0, "a")
+	checkGet(id1, "b")
+	checkGet(id2, "e")
+	checkGet(id3, "d")
+	checkRemove(id1, "b")
+	id1 = checkInsert("f")
+	checkGet(id0, "a")
+	checkGet(id1, "f")
+	checkGet(id2, "e")
+	checkGet(id3, "d")
+	checkRemove(id0, "a")
+	id0 = checkInsert("g")
+	checkGet(id0, "g")
+	checkGet(id1, "f")
+	checkGet(id2, "e")
+	checkGet(id3, "d")
+	checkRemove(id3, "d")
+	id3 = checkInsert("h")
+	checkGet(id0, "g")
+	checkGet(id1, "f")
+	checkGet(id2, "e")
+	checkGet(id3, "h")
+	checkRemove(id2, "e")
+	checkRemove(id1, "f")
+	id2 = checkInsert("i")
+	checkGet(id0, "g")
+	checkGet(id2, "i")
+	checkGet(id3, "h")
+	checkRemove(id0, "g")
+	id1 = checkInsert("j")
+	checkGet(id1, "j")
+	checkGet(id2, "i")
+	checkGet(id3, "h")
+	id0 = checkInsert("k")
+	checkRemove(id3, "h")
+	checkGet(id0, "k")
+	checkGet(id1, "j")
+	checkGet(id2, "i")
+	checkRemove(id2, "i")
+	checkRemove(id1, "j")
+	checkRemove(id0, "k")
+
+	// XXX: This is somewhat frail since it relies on
+	// internal details.
+	checkBits := func(wantLen, wantRem int) {
+		if len := len(m.ids); len != wantLen {
+			t.Fatalf("len(dataMap.ids)\nhave: %d\nwant %d", len, wantLen)
+		}
+		if len := m.idMap.Len(); len != wantLen {
+			t.Fatalf("dataMap.idMap.Len()\nhave: %d\nwant %d", len, wantLen)
+		}
+		if rem := m.idMap.Rem(); rem != wantRem {
+			t.Fatalf("dataMap.idMap.Rem()\nhave: %d\nwant %d", rem, wantRem)
+		}
+	}
+
+	checkLen(0)
+	checkBits(32, 32)
+	ids := make([]id, 32)
+	for i := range ids {
+		ids[i] = checkInsert(strconv.Itoa(i))
+	}
+	checkBits(32, 0)
+	for i, id := range ids {
+		checkRemove(id, strconv.Itoa(i))
+	}
+	checkBits(32, 32)
+	for i := range ids {
+		ids[i] = checkInsert(strconv.Itoa(i))
+	}
+	checkBits(32, 0)
+	ids = append(ids, checkInsert("32"))
+	checkBits(64, 31)
+	for i, id := range ids {
+		checkRemove(id, strconv.Itoa(i))
+	}
+
+	checkLen(0)
+	checkBits(64, 64)
+	ids = append(ids, make([]id, 256-len(ids))...)
+	for i := range ids {
+		ids[i] = checkInsert(strconv.Itoa(i))
+	}
+	checkBits(256, 0)
+	for i, id := range ids {
+		checkRemove(id, strconv.Itoa(i))
+	}
+	checkBits(256, 256)
+	id0 = checkInsert("0")
+	checkBits(256, 255)
+	for i := range ids {
+		ids[i] = checkInsert(strconv.Itoa(i + 1))
+	}
+	checkBits(512, 255)
+	for i, id := range ids {
+		checkRemove(id, strconv.Itoa(i+1))
+	}
+	checkBits(512, 511)
+	checkLen(1)
+	checkRemove(id0, "0")
+	checkBits(512, 512)
 }
 
 func TestLight(t *testing.T) {
