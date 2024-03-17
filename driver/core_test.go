e0cccd30171969bf4bdd96ddf0a8c5170297e285
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2024-03-17 14:47:34 -0300

    driver/core_test.go: add TestCmdBuffer

--- driver/core_test.go
+++ driver/core_test.go
@@ -13,3 +13,35 @@ func TestGPUDriver(t *testing.T) {
 		t.Error("GPU.Driver: unexpected Driver value")
 	}
 }
+
+func TestCmdBuffer(t *testing.T) {
+	cb, err := gpu.NewCmdBuffer()
+	if err != nil {
+		t.Errorf("GPU.NewCmdBuffer failed: %#v", err)
+		return
+	}
+	defer cb.Destroy()
+	for range 2 {
+		if cb.IsRecording() {
+			t.Error("CmdBuffer.isRecording:\nhave true\nwant false")
+		}
+		err := cb.Begin()
+		if err != nil {
+			t.Errorf("CmdBuffer.Begin failed: %#v", err)
+			return
+		}
+		if !cb.IsRecording() {
+			t.Error("CmdBuffer.isRecording:\nhave false\nwant true")
+		}
+		err = cb.End()
+		if err != nil {
+			t.Errorf("CmdBuffer.End failed: %#v", err)
+			return
+		}
+		err = cb.Reset()
+		if err != nil {
+			t.Errorf("CmdBuffer.Reset failed: %#v", err)
+			return
+		}
+	}
+}
