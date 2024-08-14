bc6c4429aa2165ded4d93eea4e607a1ddd19f34a
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2024-08-14 16:37:58 -0300

    driver/example_draw_test.go: invert clip space y coordinate

--- driver/example_draw_test.go
+++ driver/example_draw_test.go
@@ -345,9 +345,9 @@ func Example_draw() {
 var (
 	// Vertex positions (CCW).
 	triPos = [9]float32{
-		-1, 1, 0,
-		1, 1, 0,
-		0, -1, 0,
+		-1, -1, 0,
+		1, -1, 0,
+		0, 1, 0,
 	}
 	// Vertex colors.
 	triCol = [12]float32{
