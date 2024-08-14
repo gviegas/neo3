b307fcbdd46df191957b3604ee4cca9430b90698
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2024-08-14 18:27:00 -0300

    driver/example_dispatch_test.go: minor changes

--- driver/example_dispatch_test.go
+++ driver/example_dispatch_test.go
@@ -26,31 +26,29 @@ func Example_dispatch() {
 		Width:  grpCntX * invCntX,
 		Height: grpCntY * invCntY,
 	}
-	pfmt := driver.RGBA8un // From shader code.
-	size := int64(dim.Width * dim.Height * pfmt.Size())
+	pf := driver.RGBA8un // From shader code.
+	sz := int64(dim.Width * dim.Height * pf.Size())
 
 	// Create the storage image/view (write-only).
-	storage, err := gpu.NewImage(pfmt, dim, 1, 1, 1, driver.UCopySrc|driver.UShaderWrite)
+	stor, err := gpu.NewImage(pf, dim, 1, 1, 1, driver.UCopySrc|driver.UShaderWrite)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer storage.Destroy()
-	sview, err := storage.NewView(driver.IView2D, 0, 1, 0, 1)
+	defer stor.Destroy()
+	view, err := stor.NewView(driver.IView2D, 0, 1, 0, 1)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer sview.Destroy()
+	defer view.Destroy()
 
 	// Create the descriptor heap/table that will
 	// contain the storage view.
-	dheap, err := gpu.NewDescHeap([]driver.Descriptor{
-		{
-			Type:   driver.DImage,
-			Stages: driver.SCompute,
-			Nr:     0,
-			Len:    1,
-		},
-	})
+	dheap, err := gpu.NewDescHeap([]driver.Descriptor{{
+		Type:   driver.DImage,
+		Stages: driver.SCompute,
+		Nr:     0,
+		Len:    1,
+	}})
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -65,7 +63,7 @@ func Example_dispatch() {
 	if err := dheap.New(1); err != nil {
 		log.Fatal(err)
 	}
-	dheap.SetImage(0, 0, 0, []driver.ImageView{sview}, nil)
+	dheap.SetImage(0, 0, 0, []driver.ImageView{view}, nil)
 
 	// Get the compute shader.
 	// This shader has (invCntX, invCntY, 1) invocations
@@ -94,25 +92,25 @@ func Example_dispatch() {
 	cs := b.Bytes()
 
 	// Create the compute pipeline.
-	cpl, err := gpu.NewPipeline(&driver.CompState{
+	pl, err := gpu.NewPipeline(&driver.CompState{
 		Func: driver.ShaderFunc{cs, shd.funcName},
 		Desc: dtab,
 	})
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer cpl.Destroy()
+	defer pl.Destroy()
 
-	// Create a staging buffer to copy the result,
-	// so it can be read by the CPU.
-	staging, err := gpu.NewBuffer(size, true, driver.UCopyDst)
+	// Create a staging buffer to copy the result
+	// into, so it can be accessed by the CPU.
+	stg, err := gpu.NewBuffer(sz, true, driver.UCopyDst)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer staging.Destroy()
+	defer stg.Destroy()
 
 	// We will record the dispatch and the copy in
-	// different command buffers.
+	// separate command buffers.
 	cbDisp, err := gpu.NewCmdBuffer()
 	if err != nil {
 		log.Fatal(err)
@@ -133,22 +131,20 @@ func Example_dispatch() {
 			return
 		}
 		cbDisp.SetDescTableComp(dtab, 0, []int{0})
-		cbDisp.SetPipeline(cpl)
-		cbDisp.Transition([]driver.Transition{
-			{
-				Barrier: driver.Barrier{
-					SyncBefore:   driver.SNone,
-					SyncAfter:    driver.SComputeShading,
-					AccessBefore: driver.ANone,
-					AccessAfter:  driver.AShaderWrite,
-				},
-				LayoutBefore: driver.LUndefined,
-				LayoutAfter:  driver.LShaderStore,
-				Img:          storage,
-				Layers:       1,
-				Levels:       1,
+		cbDisp.SetPipeline(pl)
+		cbDisp.Transition([]driver.Transition{{
+			Barrier: driver.Barrier{
+				SyncBefore:   driver.SNone,
+				SyncAfter:    driver.SComputeShading,
+				AccessBefore: driver.ANone,
+				AccessAfter:  driver.AShaderWrite,
 			},
-		})
+			LayoutBefore: driver.LUndefined,
+			LayoutAfter:  driver.LShaderStore,
+			Img:          stor,
+			Layers:       1,
+			Levels:       1,
+		}})
 		cbDisp.Dispatch(grpCntX, grpCntY, 1)
 		if err := cbDisp.End(); err != nil {
 			ch <- err
@@ -162,26 +158,24 @@ func Example_dispatch() {
 		if err := cbCopy.Begin(); err != nil {
 			log.Fatal(err)
 		}
-		cbCopy.Transition([]driver.Transition{
-			{
-				Barrier: driver.Barrier{
-					SyncBefore:   driver.SComputeShading,
-					SyncAfter:    driver.SCopy,
-					AccessBefore: driver.AShaderWrite,
-					AccessAfter:  driver.ACopyRead,
-				},
-				LayoutBefore: driver.LShaderStore,
-				LayoutAfter:  driver.LCopySrc,
-				Img:          storage,
-				Layers:       1,
-				Levels:       1,
+		cbCopy.Transition([]driver.Transition{{
+			Barrier: driver.Barrier{
+				SyncBefore:   driver.SComputeShading,
+				SyncAfter:    driver.SCopy,
+				AccessBefore: driver.AShaderWrite,
+				AccessAfter:  driver.ACopyRead,
 			},
-		})
+			LayoutBefore: driver.LShaderStore,
+			LayoutAfter:  driver.LCopySrc,
+			Img:          stor,
+			Layers:       1,
+			Levels:       1,
+		}})
 		cbCopy.CopyImgToBuf(&driver.BufImgCopy{
-			Buf:     staging,
+			Buf:     stg,
 			RowStrd: dim.Width,
 			SlcStrd: dim.Height,
-			Img:     storage,
+			Img:     stor,
 			Size:    dim,
 			Layers:  1,
 		})
@@ -192,7 +186,7 @@ func Example_dispatch() {
 		ch <- nil
 	}()
 
-	// Commit command buffers.
+	// Commit the command buffers.
 	for i := 0; i < cap(ch); i++ {
 		if err := <-ch; err != nil {
 			log.Fatal(err)
@@ -211,7 +205,7 @@ func Example_dispatch() {
 
 	// Write the result to file.
 	nrgba := image.NewNRGBA(image.Rect(0, 0, dim.Width, dim.Height))
-	copy(nrgba.Pix, staging.Bytes())
+	copy(nrgba.Pix, stg.Bytes())
 	file, err = os.Create("testdata/checker.png")
 	if err != nil {
 		log.Fatal(err)
