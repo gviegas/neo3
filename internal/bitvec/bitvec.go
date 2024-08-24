423dace2f92ee6647c89cc41aa2773538859e122
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2024-08-24 11:30:43 -0300

    internal: replace bitm package with bitvec

--- internal/bitvec/bitvec.go
+++ internal/bitvec/bitvec.go
@@ -1,133 +1,134 @@
 // Copyright 2023 Gustavo C. Viegas. All rights reserved.
 
-// Package bitm defines a bitmap type useful for resource management
-// (e.g., memory allocation and free list implementations).
-package bitm
+// Package bitvec defines a bit vector type useful for
+// resource management (e.g., memory allocation and
+// free list implementations).
+package bitvec
 
 import (
 	"unsafe"
 )
 
-// Uint represents the granularity of a bitmap.
+// Uint represents the granularity of a bit vector.
 type Uint interface {
 	~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr
 }
 
-// Bitm is a growable bitmap with custom granularity.
-type Bitm[T Uint] struct {
-	m   []T
+// V is a growable bit vector with custom granularity.
+type V[T Uint] struct {
+	s   []T
 	rem int
 }
 
 // nbit returns the number of bits in T.
 // TODO: This is not constant.
-func (m *Bitm[T]) nbit() int { return int(unsafe.Sizeof(T(0))) * 8 }
+func (*V[T]) nbit() int { return int(unsafe.Sizeof(T(0))) * 8 }
 
-// Len returns the number of bits in the map.
-func (m *Bitm[_]) Len() int { return len(m.m) * m.nbit() }
+// Len returns the number of bits in the vector.
+func (v *V[_]) Len() int { return len(v.s) * v.nbit() }
 
-// Rem returns the number of unset bits in the map.
-func (m *Bitm[_]) Rem() int { return m.rem }
+// Rem returns the number of unset bits in the vector.
+func (v *V[_]) Rem() int { return v.rem }
 
-// Grow resizes the map to contain nplus additional Uints.
+// Grow resizes the vector to contain nplus additional Uints.
 // The new extent will be appended as a contiguous range of
 // unset bits, such that requesting the range
 //
 //	nplus * <number of bits in T>
 //
 // is guaranteed to succeed.
-// It returns the value of m.Len prior to appending the new
+// It returns the value of v.Len prior to appending the new
 // extent, so if nplus is less than 1, this value will be
 // out of bounds.
 // It is valid to call this method with any value of nplus.
-func (m *Bitm[T]) Grow(nplus int) (index int) {
-	index = m.Len()
+func (v *V[T]) Grow(nplus int) (index int) {
+	index = v.Len()
 	if nplus > 0 {
-		m.rem += nplus * m.nbit()
-		m.m = append(m.m, make([]T, nplus)...)
+		v.rem += nplus * v.nbit()
+		v.s = append(v.s, make([]T, nplus)...)
 	}
 	return
 }
 
-// Shrink resizes the map to contain nminus less Uints.
-// This in effect truncates the map, so the bits in the range
+// Shrink resizes the vector to contain nminus less Uints.
+// This in effect truncates the vector, so the bits in the range
 //
-//	m.Len() - nminus*<number of bits in T> : m.Len()
+//	v.Len() - nminus*<number of bits in T> : v.Len()
 //
 // will be removed.
 // It is valid to call this method with any value of nminus.
-func (m *Bitm[T]) Shrink(nminus int) {
+func (v *V[T]) Shrink(nminus int) {
 	if nminus <= 0 {
 		return
 	}
-	n := len(m.m) - nminus
+	n := len(v.s) - nminus
 	if n <= 0 {
-		m.m = m.m[:0]
-		m.rem = 0
+		v.s = v.s[:0]
+		v.rem = 0
 		return
 	}
 	for i := n; i < n+nminus; i++ {
-		switch m.m[i] {
+		switch v.s[i] {
 		case 0:
-			m.rem -= m.nbit()
+			v.rem -= v.nbit()
 		case ^T(0):
 		default:
-			for x := ^m.m[i]; x != 0; x >>= 1 {
+			for x := ^v.s[i]; x != 0; x >>= 1 {
 				if x&1 == 1 {
-					m.rem--
+					v.rem--
 				}
 			}
 		}
 	}
-	m.m = m.m[:n]
+	v.s = v.s[:n]
 }
 
 // Set sets a given bit.
-func (m *Bitm[T]) Set(index int) {
-	n := m.nbit()
+func (v *V[T]) Set(index int) {
+	n := v.nbit()
 	i := index / n
 	b := T(1) << (index & (n - 1))
-	if m.m[i]&b == 0 {
-		m.m[i] |= b
-		m.rem--
+	if v.s[i]&b == 0 {
+		v.s[i] |= b
+		v.rem--
 	}
 }
 
 // Unset unsets a given bit.
-func (m *Bitm[T]) Unset(index int) {
-	n := m.nbit()
+func (v *V[T]) Unset(index int) {
+	n := v.nbit()
 	i := index / n
 	b := T(1) << (index & (n - 1))
-	if m.m[i]&b != 0 {
-		m.m[i] &^= b
-		m.rem++
+	if v.s[i]&b != 0 {
+		v.s[i] &^= b
+		v.rem++
 	}
 }
 
 // IsSet checks whether a given bit is set.
-func (m *Bitm[T]) IsSet(index int) bool {
-	n := m.nbit()
+func (v *V[T]) IsSet(index int) bool {
+	n := v.nbit()
 	i := index / n
 	b := T(1) << (index & (n - 1))
-	return m.m[i]&b != 0
+	return v.s[i]&b != 0
 }
 
-// Search attempts to locate an unset bit in the map.
+// Search attempts to locate an unset bit in the vector.
 // If ok is true, then index is a value suitable for use in
-// a call to m.Set.
-// This method will fail only when m.Rem() == 0.
-func (m *Bitm[T]) Search() (index int, ok bool) {
-	if m.Rem() == 0 {
+// a call to v.Set.
+// This method will fail only when v.Rem() == 0.
+func (v *V[T]) Search() (index int, ok bool) {
+	if v.Rem() == 0 {
 		return
 	}
-	for i, x := range m.m {
+	for i, x := range v.s {
 		if x == ^T(0) {
 			continue
 		}
 		var b int
 		for ; x&(1<<b) != 0; b++ {
 		}
-		index = i*m.nbit() + b
+		index = i*v.nbit() + b
 		ok = true
 		break
 	}
@@ -136,39 +137,39 @@ func (m *Bitm[T]) Search() (index int, ok bool) {
 
 // SearchRange attempts to locate a contiguous range of unset bits.
 // If ok is true, then all values in the range [index, index + n)
-// are suitable for use in a call to m.Set.
+// are suitable for use in a call to v.Set.
 // It calls Search if n <= 1.
-func (m *Bitm[T]) SearchRange(n int) (index int, ok bool) {
+func (v *V[T]) SearchRange(n int) (index int, ok bool) {
 	if n <= 1 {
-		return m.Search()
+		return v.Search()
 	}
-	if m.Rem() < n {
+	if v.Rem() < n {
 		return
 	}
-	nb := m.nbit()
+	nb := v.nbit()
 	var cnt, idx, bit, i int
 	for {
 		// Skip Uints that have no unset bits.
-		if m.m[i] == ^T(0) {
+		if v.s[i] == ^T(0) {
 			cnt, bit = 0, 0
 			i++
-			for ; i < len(m.m); i++ {
-				if m.m[i] != ^T(0) {
+			for ; i < len(v.s); i++ {
+				if v.s[i] != ^T(0) {
 					break
 				}
 			}
 			idx = i
 		}
 		// Give up if there is not enough bits left.
-		if cnt+nb*(len(m.m)-i) < n {
+		if cnt+nb*(len(v.s)-i) < n {
 			return
 		}
 		// Iterate over whole Uints as much as possible.
-		if m.m[i] == 0 {
+		if v.s[i] == 0 {
 			cnt += nb
 			i++
 			for j := 0; j < (n-cnt)/nb; j++ {
-				if m.m[i+j] != 0 {
+				if v.s[i+j] != 0 {
 					cnt += j * nb
 					i += j
 					break
@@ -192,7 +193,7 @@ func (m *Bitm[T]) SearchRange(n int) (index int, ok bool) {
 		//    with subsequent Uint(s).
 		//
 		for j := 0; j < nb; j++ {
-			if m.m[i]&(1<<j) == 0 {
+			if v.s[i]&(1<<j) == 0 {
 				cnt++
 				if cnt >= n {
 					index = idx*nb + bit
@@ -211,19 +212,19 @@ func (m *Bitm[T]) SearchRange(n int) (index int, ok bool) {
 			}
 		}
 		i++
-		if i == len(m.m) {
+		if i == len(v.s) {
 			break
 		}
 	}
 	return
 }
 
-// Clear unsets every bit in the map.
-func (m *Bitm[T]) Clear() {
-	n := m.Len()
-	if n == m.Rem() {
+// Clear unsets every bit in the vector.
+func (v *V[T]) Clear() {
+	n := v.Len()
+	if n == v.Rem() {
 		return
 	}
-	clear(m.m)
-	m.rem = n
+	clear(v.s)
+	v.rem = n
 }
---
+++ internal/bitvec/bitvec_test.go
@@ -0,0 +1,446 @@
+// Copyright 2023 Gustavo C. Viegas. All rights reserved.
+
+package bitvec
+
+import (
+	"strconv"
+	"testing"
+	"unsafe"
+)
+
+func TestNbit(t *testing.T) {
+	for _, x := range [...][2]int{
+		{int(unsafe.Sizeof(uint(0))) * 8, (&V[uint]{}).nbit()},
+		{int(unsafe.Sizeof(uint8(0))) * 8, (&V[uint8]{}).nbit()},
+		{int(unsafe.Sizeof(uint16(0))) * 8, (&V[uint16]{}).nbit()},
+		{int(unsafe.Sizeof(uint32(0))) * 8, (&V[uint32]{}).nbit()},
+		{int(unsafe.Sizeof(uint64(0))) * 8, (&V[uint64]{}).nbit()},
+		{int(unsafe.Sizeof(uintptr(0))) * 8, (&V[uintptr]{}).nbit()},
+	} {
+		if x[0] != x[1] {
+			t.Fatalf("V[T].nbit:\nhave %d\nwant %d", x[0], x[1])
+		}
+	}
+}
+
+func TestZero(t *testing.T) {
+	var v16 V[uint16]
+	if v16.s != nil {
+		t.Fatalf("v16.s:\nhave %d\nwant nil", v16.s)
+	}
+	if v16.rem != 0 {
+		t.Fatalf("v16.rem:\nhave %d\nwant 0", v16.rem)
+	}
+	if n := v16.Len(); n != 0 {
+		t.Fatalf("v16.Len:\nhave %d\nwant 0", n)
+	}
+	if n := v16.Rem(); n != 0 {
+		t.Fatalf("v16.Rem:\nhave %d\nwant 0", n)
+	}
+}
+
+func TestGrow(t *testing.T) {
+	var v32 V[uint32]
+	for _, x := range [...]struct {
+		nplus, wantLen int
+	}{
+		{1, 32},
+		{2, 96},
+		{3, 192},
+		{0, 192},
+		{16, 704},
+		{17, 1248},
+		{32, 2272},
+		{99, 5440},
+		{-1, 5440},
+		{32768, 1054016},
+	} {
+		if n, i := v32.Len(), v32.Grow(x.nplus); n != i {
+			t.Fatalf("v32.Grow:\nhave %d\nwant %d", i, n)
+		}
+		if n := v32.Len(); n != x.wantLen {
+			t.Fatalf("v32.Grow: Len:\nhave %d\nwant %d", n, x.wantLen)
+		}
+		if n := v32.Rem(); n != x.wantLen {
+			t.Fatalf("v32.Grow: Rem:\nhave %d\nwant %d", n, x.wantLen)
+		}
+		for i, x := range v32.s {
+			if x != 0 {
+				t.Fatalf("v32.s[%d]:\nhave %d\nwant 0", i, x)
+			}
+		}
+	}
+}
+
+func TestShrink(t *testing.T) {
+	var v8 V[uint8]
+	checkLenRem := func(len, rem int) {
+		if n := v8.Len(); n != len {
+			t.Fatalf("v8.Shrink: Len:\nhave %d\nwant %d", n, len)
+		}
+		if n := v8.Rem(); n != rem {
+			t.Fatalf("v8.Shrink: Rem:\nhave %d\nwant %d", n, rem)
+		}
+	}
+	for _, x := range [...]int{0, 1, -1, 2, 100} {
+		v8.Shrink(x)
+		checkLenRem(0, 0)
+	}
+	v8.Grow(1)
+	v8.Shrink(1)
+	checkLenRem(0, 0)
+	v8.Grow(1)
+	v8.Set(0)
+	v8.Shrink(1)
+	checkLenRem(0, 0)
+	v8.Grow(2)
+	v8.Shrink(1)
+	checkLenRem(8, 8)
+	v8.Grow(2)
+	v8.Shrink(1)
+	checkLenRem(16, 16)
+	v8.Shrink(2)
+	checkLenRem(0, 0)
+	v8.Grow(2)
+	v8.Set(0)
+	v8.Shrink(1)
+	checkLenRem(8, 7)
+	v8.Shrink(1)
+	checkLenRem(0, 0)
+	v8.Grow(2)
+	v8.Set(1)
+	v8.Set(7)
+	v8.Set(4)
+	v8.Set(0)
+	v8.Shrink(0)
+	checkLenRem(16, 12)
+	v8.Shrink(1)
+	checkLenRem(8, 4)
+	v8.Grow(2)
+	v8.Set(10)
+	v8.Set(15)
+	v8.Shrink(-1)
+	checkLenRem(24, 18)
+	v8.Shrink(1)
+	checkLenRem(16, 10)
+	v8.Grow(1)
+	v8.Set(23)
+	v8.Shrink(^0)
+	checkLenRem(24, 17)
+	v8.Shrink(2)
+	checkLenRem(8, 4)
+	v8.Unset(0)
+	v8.Grow(10)
+	v8.Set(79)
+	v8.Shrink(9)
+	checkLenRem(16, 13)
+	v8.Grow(8)
+	for i := 0; i < v8.Len(); i++ {
+		v8.Set(i)
+	}
+	v8.Shrink(0)
+	checkLenRem(80, 0)
+	for i := 72; i >= 0; i -= 8 {
+		v8.Shrink(1)
+		checkLenRem(i, 0)
+	}
+	v8.Grow(10)
+	for i := 80; i > -3; i -= 8 {
+		checkLenRem(i, i)
+		v8.Shrink(1)
+	}
+	v8.Grow(100)
+	for i := 0; i < v8.Len(); i += 2 {
+		v8.Set(i)
+	}
+	for i := 0; i <= 100; i += 2 {
+		checkLenRem(800-i*8, 400-i*4)
+		v8.Shrink(2)
+	}
+	checkLenRem(0, 0)
+}
+
+// check represents an expected V.s[index] value.
+type check[T Uint] struct {
+	index int
+	want  T
+}
+
+// checkState checks the state of v.s against a set of expected values.
+func (v *V[T]) checkState(values []check[T], t *testing.T) {
+	for _, x := range values {
+		if y := v.s[x.index]; y != x.want {
+			t.Fatalf("v.s[%d]:\nhave 0x%x\nwant 0x%x", x.index, y, x.want)
+		}
+	}
+}
+
+// checkRem checks that v.Rem() matches the state of v.s.
+func (v *V[T]) checkRem(t *testing.T) {
+	want := v.Len()
+	n := v.nbit()
+	for _, x := range v.s {
+		for i := 0; i < n; i++ {
+			if x&(1<<i) != 0 {
+				want--
+			}
+		}
+	}
+	if r := v.Rem(); r != want {
+		t.Fatalf("v.Rem:\nhave %d\nwant %d", r, want)
+	}
+}
+
+func TestSetUnset(t *testing.T) {
+	var v8 V[uint8]
+	v8.Grow(1)
+	v8.Set(6)
+	v8.checkState([]check[uint8]{{0, 0x40}}, t)
+	v8.Set(1)
+	v8.checkState([]check[uint8]{{0, 0x42}}, t)
+	v8.checkRem(t)
+	v8.Unset(6)
+	v8.checkState([]check[uint8]{{0, 0x02}}, t)
+	v8.checkRem(t)
+	v8.Set(6)
+	v8.checkState([]check[uint8]{{0, 0x42}}, t)
+	v8.Grow(2)
+	v8.checkState([]check[uint8]{{0, 0x42}, {1, 0}, {2, 0}}, t)
+	v8.Set(10)
+	v8.checkState([]check[uint8]{{0, 0x42}, {1, 0x04}, {2, 0}}, t)
+	v8.Unset(1)
+	v8.checkState([]check[uint8]{{0, 0x40}, {1, 0x04}, {2, 0}}, t)
+	v8.Set(21)
+	v8.checkState([]check[uint8]{{0, 0x40}, {1, 0x04}, {2, 0x20}}, t)
+	v8.Set(21)
+	v8.Unset(23)
+	v8.Unset(0)
+	v8.checkState([]check[uint8]{{0, 0x40}, {1, 0x04}, {2, 0x20}}, t)
+	v8.checkRem(t)
+	v8.Set(4)
+	v8.Set(14)
+	v8.Set(16)
+	v8.checkState([]check[uint8]{{0, 0x50}, {1, 0x44}, {2, 0x21}}, t)
+	for i := 0; i < v8.Len(); i++ {
+		if i&3 == 0 {
+			v8.Set(i)
+		} else {
+			v8.Unset(i)
+		}
+	}
+	v8.checkState([]check[uint8]{{0, 0x11}, {1, 0x11}, {2, 0x11}}, t)
+	v8.checkRem(t)
+}
+
+func TestIsSet(t *testing.T) {
+	var v64 V[uint64]
+	v64.Grow(2)
+	checkUnset := func(start, end int) {
+		for i := start; i < end; i++ {
+			if v64.IsSet(i) {
+				t.Fatalf("v64.isSet: %d:\nhave true\nwant false", i)
+			}
+		}
+	}
+	checkSet := func(start, end int) {
+		for i := start; i < end; i++ {
+			if !v64.IsSet(i) {
+				t.Fatalf("v64.isSet: %d:\nhave false\nwant true", i)
+			}
+		}
+	}
+	checkUnset(0, v64.Len())
+	v64.Set(0)
+	checkSet(0, 1)
+	checkUnset(1, v64.Len())
+	v64.Set(1)
+	checkSet(0, 2)
+	v64.Unset(0)
+	checkUnset(0, 1)
+	checkSet(1, 2)
+	v64.Set(v64.Len() - 1)
+	checkSet(v64.Len()-1, v64.Len())
+	for i := 0; i < v64.Len(); i++ {
+		v64.Unset(i)
+	}
+	checkUnset(0, v64.Len())
+	for i := 0; i < v64.Len(); i++ {
+		v64.Set(i)
+	}
+	checkSet(0, v64.Len())
+}
+
+// checkSearch calls v.Search and checks the expected result.
+// If want < 0, then Search must fail.
+func (v *V[_]) checkSearch(want int, t *testing.T) {
+	index, ok := v.Search()
+	if want < 0 {
+		if ok {
+			t.Fatalf("v.Search: \nhave %d, true\nwant _, false", index)
+		}
+	} else {
+		if !ok {
+			t.Fatalf("v.Search: \nhave _, false\nwant %d, true", want)
+		}
+		if index != want {
+			t.Fatalf("v.Search: index:\nhave %d\nwant %d", index, want)
+		}
+	}
+}
+
+func TestSearch(t *testing.T) {
+	var v32 V[uint32]
+	v32.checkSearch(-1, t)
+	v32.Grow(12)
+	v32.checkSearch(0, t)
+	v32.Set(0)
+	v32.checkSearch(1, t)
+	v32.Set(1)
+	v32.checkSearch(2, t)
+	v32.Set(3)
+	v32.checkSearch(2, t)
+	v32.Unset(1)
+	v32.checkSearch(1, t)
+	v32.Unset(0)
+	v32.checkSearch(0, t)
+	for i := 0; i < v32.nbit()*2; i++ {
+		v32.Set(i)
+	}
+	v32.checkSearch(64, t)
+	for i := 64; i < v32.Len(); i++ {
+		v32.Set(i)
+	}
+	v32.checkSearch(-1, t)
+	v32.Unset(120)
+	v32.checkSearch(120, t)
+}
+
+// checkSearchRange calls v.SearchRange and checks the expected result.
+// If want < 0, then SearchRange must fail.
+func (v *V[_]) checkSearchRange(n, want int, t *testing.T) {
+	index, ok := v.SearchRange(n)
+	if want < 0 {
+		if ok {
+			t.Fatalf("v.SearchRange: \nhave %d, true\nwant _, false", index)
+		}
+	} else {
+		if !ok {
+			t.Fatalf("v.SearchRange: \nhave _, false\nwant %d, true", want)
+		}
+		if index != want {
+			t.Fatalf("v.SearchRange: index:\nhave %d\nwant %d", index, want)
+		}
+	}
+}
+
+func TestSearchRange(t *testing.T) {
+	var v16 V[uint16]
+	setRange := func(start, end int) {
+		for i := start; i < end; i++ {
+			v16.Set(i)
+		}
+	}
+	v16.checkSearchRange(3, -1, t)
+	v16.Grow(4)
+	v16.checkSearchRange(3, 0, t)
+	setRange(0, 3)
+	v16.checkSearchRange(3, 3, t)
+	setRange(3, 6)
+	v16.checkSearchRange(3, 6, t)
+	setRange(6, 9)
+	v16.checkSearchRange(1, 9, t)
+	v16.Set(9)
+	v16.checkSearchRange(2, 10, t)
+	setRange(10, 12)
+	v16.Unset(1)
+	v16.checkSearchRange(2, 12, t)
+	v16.checkSearchRange(1, 1, t)
+	v16.Unset(2)
+	v16.checkSearchRange(2, 1, t)
+	v16.checkSearchRange(1, 1, t)
+	v16.checkSearchRange(6, 12, t)
+	setRange(12, 18)
+	v16.checkSearchRange(13, 18, t)
+	setRange(19, 32)
+	v16.Set(35)
+	v16.Set(46)
+	v16.checkSearchRange(4, 36, t)
+	v16.checkSearchRange(3, 32, t)
+	v16.checkSearchRange(10, 36, t)
+	v16.checkSearchRange(11, 47, t)
+	v16.checkSearchRange(20, -1, t)
+	v16.Grow(1)
+	v16.checkSearchRange(20, 47, t)
+	v16.checkSearchRange(31, 47, t)
+	v16.checkSearchRange(33, 47, t)
+	v16.checkSearchRange(34, -1, t)
+	v16.Set(76)
+	v16.checkSearchRange(20, 47, t)
+	v16.checkSearchRange(31, -1, t)
+	v16.checkSearchRange(33, -1, t)
+	v16.checkSearchRange(34, -1, t)
+	v16.Grow(5)
+	v16.checkSearchRange(80, 77, t)
+	v16.Set(79)
+	v16.checkSearchRange(80, 80, t)
+	v16.Set(80)
+	v16.checkSearchRange(80, -1, t)
+	v16.checkSearchRange(79, 81, t)
+}
+
+func TestClear(t *testing.T) {
+	var vu V[uint]
+	checkClear := func() {
+		if vu.Len() != vu.Rem() {
+			t.Fatal("vu.Clear: Len == Rem\nhave false\nwant true")
+
+		}
+		for i, x := range vu.s {
+			if x != 0 {
+				t.Fatalf("vu.Clear: m[%d]\nhave %d\nwant 0", i, x)
+			}
+		}
+	}
+	checkClear()
+	vu.Grow(1)
+	checkClear()
+	for i := 0; i < vu.Len(); i++ {
+		vu.Set(i)
+	}
+	vu.Clear()
+	checkClear()
+	vu.Grow(9)
+	checkClear()
+	for i := 0; i < vu.Len(); i++ {
+		vu.Set(i)
+	}
+	vu.Clear()
+	checkClear()
+	for i := vu.nbit(); i < vu.Len(); i += 3 {
+		vu.Set(i)
+	}
+	vu.Clear()
+	checkClear()
+	for i := vu.nbit(); i < vu.Len()-vu.nbit(); i++ {
+		vu.Set(i)
+	}
+	vu.Clear()
+	checkClear()
+}
+
+// printVec is for debug printing of V.s.
+func printVec[T Uint](v *V[T]) {
+	n := v.nbit()
+	s := "\n"
+	for i, x := range v.s {
+		for i := 0; i < n; i++ {
+			if x&(1<<i) != 0 {
+				s += "1 "
+			} else {
+				s += "0 "
+			}
+		}
+		s += " " + strconv.Itoa(i*n) + ":" + strconv.Itoa(i*n+n-1) + "\n"
+	}
+	print(s)
+}
