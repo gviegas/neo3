eedbbf88960aca729086e436ef3d18c59a3f4b7c
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2024-11-10 15:26:41 -0300

    wsi/keymap.go: update doc

--- wsi/keymap.go
+++ wsi/keymap.go
@@ -4,13 +4,13 @@ package wsi
 
 // keyFrom returns the Key value that represents an
 // OS-specific key code.
+// It assumes that code is greater than or equal to
+// zero.
 // Every supported system must provide an indexable
 // var named keymap that contains Key values.
 //
-// Note: If keymap is implemented as a map type,
-// its length must be greater than the maximum
-// key code value. Also, do not implement keymap
-// as a map type.
+// NOTE: keymap must be indexable by non-negative
+// values up to its length minus one.
 func keyFrom(code int) Key {
 	if code >= len(keymap) {
 		return KeyUnknown
